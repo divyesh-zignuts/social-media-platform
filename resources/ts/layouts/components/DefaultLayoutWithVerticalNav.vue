d23d133bdfabd934a8d80e38f4f94c6c8779ce4c
Divyesh Mepal <divyeshm@zignuts.com>
2024-03-27 14:40:00 +0530

    Show only accessible sidebar link based on user role

--- resources/ts/layouts/components/DefaultLayoutWithVerticalNav.vue
+++ resources/ts/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import navItems from '@/navigation/vertical'
 import { useThemeConfig } from '@core/composable/useThemeConfig'
+import CryptoJs from 'crypto-js'
 
 // Components
 import Footer from '@/layouts/components/Footer.vue'
@@ -12,23 +13,33 @@ import { VerticalNavLayout } from '@layouts'
 
 const { appRouteTransition, isLessThanOverlayNavBreakpoint } = useThemeConfig()
 const { width: windowWidth } = useWindowSize()
+
+// Computed
+const filterAccessedList = computed(() => {
+  let role = ''
+
+  const encryptedData = localStorage.getItem('userData')
+  const userData = encryptedData ? JSON.parse(CryptoJs.AES.decrypt(encryptedData || '', import.meta.env.VITE_CRYPTO_SECURE_KEY).toString(CryptoJs.enc.Utf8)) : null
+
+  if (userData)
+    console.log('userData: ', userData);
+  role = userData.role === 'A' ? 'admin' : 'user'
+
+  // check and return only list which are accessBy role-wise
+  return navItems.filter((item: any) => {
+    return item.accessBy.includes(role)
+  })
+})
 </script>
 
 <template>
-  <VerticalNavLayout :nav-items="navItems">
+  <VerticalNavLayout :nav-items="filterAccessedList">
     <!-- 👉 navbar -->
     <template #navbar="{ toggleVerticalOverlayNavActive }">
       <div class="d-flex h-100 align-center">
-        <IconBtn
-          v-if="isLessThanOverlayNavBreakpoint(windowWidth)"
-          id="vertical-nav-toggle-btn"
-          class="ms-n3"
-          @click="toggleVerticalOverlayNavActive(true)"
-        >
-          <VIcon
-            size="26"
-            icon="tabler-menu-2"
-          />
+        <IconBtn v-if="isLessThanOverlayNavBreakpoint(windowWidth)" id="vertical-nav-toggle-btn" class="ms-n3"
+          @click="toggleVerticalOverlayNavActive(true)">
+          <VIcon size="26" icon="tabler-menu-2" />
         </IconBtn>
 
         <NavbarThemeSwitcher />
@@ -41,10 +52,7 @@ const { width: windowWidth } = useWindowSize()
 
     <!-- 👉 Pages -->
     <RouterView v-slot="{ Component }">
-      <Transition
-        :name="appRouteTransition"
-        mode="out-in"
-      >
+      <Transition :name="appRouteTransition" mode="out-in">
         <Component :is="Component" />
       </Transition>
     </RouterView>
--- resources/ts/navigation/vertical/index.ts
+++ resources/ts/navigation/vertical/index.ts
@@ -5,15 +5,18 @@ export default [
     title: 'Home',
     to: { name: 'index' },
     icon: { icon: 'tabler-smart-home' },
+    accessBy: ['admin', 'user'],
   },
   {
     title: 'Users',
     to: { name: 'user-list' },
     icon: { icon: 'tabler-users' },
+    accessBy: ['admin'],
   },
   {
     title: 'Posts',
     to: { name: 'post-list' },
     icon: { icon: 'tabler-wallpaper' },
+    accessBy: ['admin','user'],
   },
-] as VerticalNavItems
+] as unknown as  VerticalNavItems
